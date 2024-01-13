f81697414526ccedff1110b377c37dcb6371b0c5
zul <salamizulkifli23@icloud.com>
2024-01-12 20:47:40 -0500

    edit behaviour layer to implement site functionalities

--- list.js
+++ list.js
@@ -1,19 +1,28 @@
 // ADDING ITEMS TO START AND END OF LIST
 // Get the <ul> element
+let list = document.getElementsByTagName('ul')[0];
 
-// ADD NEW ITEM TO END OF LIST
+/* ADD NEW ITEM TO END OF LIST
 // Create element
 // Create text node
 // Add text node to element
 // Add element end of list
+*/
+let newListItemEnd = document.createElement('li');
+let newListItemEndText = document.createTextNode('cream');
+newListItemEnd.appendChild(newListItemEndText);
+list.appendChild(newListItemEnd);
 
-// ADD NEW ITEM START OF LIST
+/* ADD NEW ITEM START OF LIST
 // Create element
 // Create text node
 // Add text node to element
 // Add element to list
-
-
+*/
+let newListItemStart = document.createElement('li');
+let newListItemStartText = document.createTextNode('kale');
+newListItemStart.appendChild(newListItemStartText);
+list.insertBefore(newListItemStart, list.firstChild);
 
 // All <li> elements
 
@@ -21,6 +30,10 @@
 // Counter variable
 // Loop through elements
 // Change class to cool
+let listItems = document.querySelectorAll('ul li');
+for (let i = 0; i < listItems.length; i++) {
+    listItems[i].className = 'cool';
+}
 
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
@@ -29,3 +42,6 @@
 // No. of <li> elements
 // Content
 // Update h2 using innerHTML (not textContent) because it contains markup
+let h2 = document.querySelector('h2');
+let listCount = listItems.length;
+h2.innerHTML = `Buy groceries <span>${listCount}</span>`;
