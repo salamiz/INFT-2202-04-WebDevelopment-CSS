17ff4ac92ec014aad0cb339abf6949ea38d71dc9
zul <salamizulkifli23@icloud.com>
2024-02-15 19:06:09 -0500

    functional implementation

--- ICE5/scripts/xhr_script.js
+++ ICE5/scripts/xhr_script.js
@@ -8,22 +8,36 @@ let url_todos = 'https://jsonplaceholder.typicode.com/todos';
 let url_users = 'https://jsonplaceholder.typicode.com/users';
 
 // instantiate the XMLHttpRequest object with the new keyword
-
+let xhr = new XMLHttpRequest();
 
 
 // create a callback function to fire when the onreadystatechange happens
+xhr.onreadystatechange = function() {
     // check that the state is done
-    // if (xhr.readyState === 4) {
-            // turn into json
-            // console log to see what we have
-            // update the 1st image
-            // update the 1st figcaption
-            // update the 2nd image
-            // update the 2nd figcaption
-            // send error message
+    if (xhr.readyState === 4) {
+        // turn into json
+        let data = JSON.parse(xhr.responseText);
+        console.log(data);
+        // update the 1st image
+        document.getElementById('photo1').src = data[0].url;
+        // update the 1st figcaption
+        document.getElementById('caption1').innerText = data[0].title;
+        // update the 2nd image
+        document.getElementById('photo2').src = data[1].url;
+        // update the 2nd figcaption
+        document.getElementById('caption2').innerText = data[1].title;
+    }
+    // send error message
+    else {
+        console.log('There was a problem with the request.');
+    }
+}
 
 // use the .open() method to configure the object
+xhr.open('GET', url_photos, true);
 
 // add datatype to header
+xhr.setRequestHeader('Accept', 'application/json');
 
 // use the .send() method to send the request
+xhr.send();
